689985ca3e10f9f1c670101b5d50ff22b20ab050
river <river@white.cn>
2018-12-16 17:18:55 +0800

    修改hello

--- Program/src/com/chinasoft/HelloWorld.java
+++ Program/src/com/chinasoft/HelloWorld.java
@@ -5,4 +5,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+
+    public static void hello() {
+        System.out.println("world");
+    }
 }
